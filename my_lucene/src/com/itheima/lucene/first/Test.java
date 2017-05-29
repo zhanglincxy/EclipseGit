44edb35639038c9b09fbad803bee6eaf9f514e6b
zl <747507073@qq.com>
2017-05-29 17:03:29 +0800

    dev 第二次提交

--- my_lucene/src/com/itheima/lucene/first/Test.java
+++ my_lucene/src/com/itheima/lucene/first/Test.java
@@ -3,5 +3,6 @@ package com.itheima.lucene.first;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("git test master");
+		System.out.println("git test master2");
 	}
 }
